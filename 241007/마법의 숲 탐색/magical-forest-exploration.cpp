64ed705d625a24a89a7fa0fd703879e102d4758c
kimye0808 <97530986+kimye0808@users.noreply.github.com>
2024-10-07 14:58:02 +0900

    Title: 마법의 숲 탐색, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241007/마법의 숲 탐색/magical-forest-exploration.cpp
+++ 241007/마법의 숲 탐색/magical-forest-exploration.cpp
@@ -7,144 +7,108 @@ int dx[4] = { 1, 0, -1, 0 };
 int dy[4] = { 0, 1, 0, -1 };
 
 pair<int, int> gol[5] = {
-    {0,0}, //원점
-    {0,1}, //오른쪽
-    {-1,0}, //아랫쪽
-    {0,-1}, //왼쪽
-    {1,0}  //윗쪽
+    {0, 0},  // 원점
+    {0, 1},  // 오른쪽
+    {-1, 0}, // 아랫쪽
+    {0, -1}, // 왼쪽
+    {1, 0}   // 윗쪽
 };
 
-bool rotate_gol(pair<int, int> (&gtmp)[5], vector<vector<bool>>& vis, int& exitdir,
-    int lr, int r, int c) {
-    //1) 왼쪽 or 오른쪽 이동
+// 골렘 회전 함수
+bool rotate_gol(pair<int, int>(&gtmp)[5], vector<vector<bool>>& vis, int& exitdir, int lr, int r, int c) {
+    // 왼쪽 또는 오른쪽으로 회전하는 경우
     for (int i = 0; i < 5; i++) {
         int lrx = gtmp[i].first;
-        int lry = gtmp[i].second + lr;//lr 이 -1이면 left, lr이 +1이면 right
+        int lry = gtmp[i].second + lr;
 
-        if (lry < 1 || lry > c) {
-            return false;
-        }
-
-        if (vis[lrx][lry]) {
+        if (lry < 1 || lry > c || vis[lrx][lry]) {
             return false;
         }
     }
 
-    //2) 아래로 이동
+    // 아래로 이동 가능한지 체크
     for (int i = 0; i < 5; i++) {
         int lrx = gtmp[i].first - 1;
         int lry = gtmp[i].second + lr;
 
-        if (lrx < 1 || lry < 1 || lry > c) {
-            return false;
-        }
-
-        if (vis[lrx][lry]) {
+        if (lrx < 1 || lry < 1 || lry > c || vis[lrx][lry]) {
             return false;
         }
     }
 
     // 좌표 갱신
     for (int i = 0; i < 5; i++) {
-        gtmp[i].first = gtmp[i].first - 1;
-        gtmp[i].second = gtmp[i].second + lr;
+        gtmp[i].first -= 1;
+        gtmp[i].second += lr;
     }
-    //반시계 방향
-    if (lr == -1) {
-        exitdir = (exitdir - 1 + 4) % 4;
-    }
-    else if(lr== 1){
-        exitdir = (exitdir + 1 + 4) % 4;
-    }
+
+    // 회전 방향에 따라 exitdir 조정
+    exitdir = (exitdir + (lr == -1 ? -1 : 1) + 4) % 4;
     return true;
 }
 
-bool move_gol(vector<vector<int>>& maps, vector<vector<bool>>& vis, int& sum,
-    int r, int c, int col, int d) {
+// 골렘 이동 처리 함수
+bool move_gol(vector<vector<int>>& maps, vector<vector<bool>>& vis, int& sum, int r, int c, int col, int d) {
+    pair<int, int> gtmp[5] = { {0, 0}, {0, 1}, {-1, 0}, {0, -1}, {1, 0} };
+    int exitdir = d;
 
-    pair<int, int> gtmp[5] = {
-        {0,0},
-        {0,1},
-        {-1,0},
-        {0,-1},
-        {1,0}
-    };
-    int exitdir = d;//0,1,2,3
-
+    // 시작 좌표 설정
     for (int i = 0; i < 5; i++) {
         gtmp[i].first = gol[i].first + r + 2;
         gtmp[i].second = gol[i].second + col;
     }
 
-    int rcnt = 0;
-    while (1) {
-        int rx = gtmp[1].first - 1;
-        int ry = gtmp[1].second;
-        if (rx < 1) break;
-        bool rvis = vis[rx][ry];
-
+    // 골렘이 아래로 이동하면서 체크
+    while (true) {
         int bx = gtmp[2].first - 1;
-        int by = gtmp[2].second;
-        if (bx < 1) break;
-        bool bvis = vis[bx][by];
+        if (bx < 1) break;  // 숲을 벗어나면 중단
 
-        int lx = gtmp[3].first - 1;
-        int ly = gtmp[3].second;
-        if (lx < 1) break;
-        bool lvis = vis[lx][ly];
-        //cout << "bx: " << bx << "by: "<<by << '\n';
+        bool canMove = !vis[gtmp[1].first - 1][gtmp[1].second] &&
+            !vis[gtmp[2].first - 1][gtmp[2].second] &&
+            !vis[gtmp[3].first - 1][gtmp[3].second];
 
-        //cout << "l: " << lvis<<"b: "<<bvis<<"r: "<< rvis << '\n';
-
-        if (!lvis && !bvis && !rvis) {//아래로 이동할 수 있으면
+        // 아래로 이동 가능한 경우
+        if (canMove) {
             for (int i = 0; i < 5; i++) {
                 gtmp[i].first -= 1;
             }
         }
-        else if (!rotate_gol(gtmp, vis, exitdir, -1, r, c)) {
-            if (!rotate_gol(gtmp, vis, exitdir, 1, r, c)) {
-                break;
+        else {
+            // 회전 시도 (왼쪽 우선, 실패하면 오른쪽)
+            if (!rotate_gol(gtmp, vis, exitdir, -1, r, c)) {
+                if (!rotate_gol(gtmp, vis, exitdir, 1, r, c)) {
+                    break;
+                }
             }
         }
-        //cout << "b: " << gtmp[1].first << '\n';
+    }
 
-    }//while(1)-이동 끝
-
-    if (gtmp[4].first > r) {//숲에 다 못들어갔다면
+    // 골렘이 숲에 다 들어가지 못한 경우
+    if (gtmp[4].first > r) {
         return false;
     }
 
-    //정상작동
-    int x = gtmp[exitdir].first;
-    int y = gtmp[exitdir].second;
+    // 블록 최하단 좌표 갱신
     int lowr = gtmp[2].first;
-
-    // 붙어있는 블록 확인
     for (int i = 0; i < 4; i++) {
-        int nx = x + dx[i];
-        int ny = y + dy[i];
-
-        if (nx<1 || ny<1 || nx>r || ny>c) continue;
-
-        if (vis[nx][ny]) {
+        int nx = gtmp[exitdir].first + dx[i];
+        int ny = gtmp[exitdir].second + dy[i];
+        if (nx >= 1 && ny >= 1 && nx <= r && ny <= c && vis[nx][ny]) {
             lowr = min(lowr, maps[nx][ny]);
         }
     }
 
-    //cout << "vis등록" << '\n';
+    // vis와 maps 갱신
     for (int i = 0; i < 5; i++) {
         int x = gtmp[i].first;
         int y = gtmp[i].second;
-
-        //cout << "x: " << x << " y: " << y << '\n';
         vis[x][y] = true;
-        maps[x][y] = lowr;//맨 밑 row값을 저장
+        maps[x][y] = lowr;
     }
-    sum += r + 1 - lowr;//1행이면 r행
-    //cout << r + 1 - lowr << '\n';
+
+    sum += r + 1 - lowr;
     return true;
 }
-
 
 int main() {
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
@@ -159,14 +123,11 @@ int main() {
         int col, d;
         cin >> col >> d;
         if (!move_gol(maps, vis, sum, r, c, col, d)) {
-            for (int j = 0; j < 100; j++) {
-                fill(maps[j].begin(), maps[j].end(), 0);
-                fill(vis[j].begin(), vis[j].end(), false);
-            }
+            fill(maps.begin(), maps.end(), vector<int>(100, 0));
+            fill(vis.begin(), vis.end(), vector<bool>(100, false));
         }
     }
 
     cout << sum;
-
     return 0;
 }
