7452e553c47c65f6494890ee2b67542b1793e2d5
kimye0808 <97530986+kimye0808@users.noreply.github.com>
2024-10-10 22:23:56 +0900

    Title: 메이즈 러너, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 241010/메이즈 러너/maze-runner.cpp
+++ 241010/메이즈 러너/maze-runner.cpp
@@ -1,245 +1,223 @@
 #include <iostream>
+#include <tuple>
 #include <vector>
 #include <algorithm>
-#include <tuple>
 using namespace std;
 
 struct Man {
 	int x, y;
-	bool done;
+	int dead;
 };
 
 int n, m, k;
-int dist_sum;
-int exitx, exity;
-vector<vector<int>> ptable(11, vector<int>(11));
 vector<vector<int>> wtable(11, vector<int>(11));
 vector<Man> man(11);
+pair<int, int> exitxy;
+int dist_sum;
 
 int dx[] = { -1, 1, 0, 0 };
 int dy[] = { 0, 0, 1, -1 };
 
+int absn(int a, int b) {
+	return a > b ? a - b : b - a;
+}
+
 int calc_dist(int fx, int fy, int tx, int ty) {
-	int x = fx - tx > 0 ? fx - tx : tx - fx;
-	int y = fy - ty > 0 ? fy - ty : ty - fy;
-	return x + y;
+	return absn(fx, tx) + absn(fy, ty);
 }
 
 void move() {
 	for (int i = 1; i <= m; i++) {
-		if (man[i].done) continue;// 이미 종료했다면
+		if (man[i].dead) continue;
 
 		int x = man[i].x;
 		int y = man[i].y;
-
-		int now_dist = calc_dist(x, y, exitx, exity);
-		int dist = calc_dist(x, y, exitx, exity);
-		int after_dir = -1;
+		int min_dist = 100;
+		int min_dir = 100;
+		int cur_dist = calc_dist(x, y, exitxy.first, exitxy.second);
 
 		for (int dir = 0; dir < 4; dir++) {
 			int nx = x + dx[dir];
 			int ny = y + dy[dir];
 
-			if (nx<1 || ny<1 || nx>n || ny>n) continue;
-			if (wtable[nx][ny]) continue;
+			if (nx < 1 || ny < 1 || nx > n || ny > n) continue;
+			if (wtable[nx][ny] > 0) continue;
 
-			int after_dist = calc_dist(nx, ny, exitx, exity);
+			int after_dist = calc_dist(nx, ny, exitxy.first, exitxy.second);
 
-			if (after_dist < dist) {
-				dist = after_dist;
-				after_dir = dir;
+			if (min_dist > after_dist) {
+				min_dist = after_dist;
+				min_dir = dir;
 			}
 		}
 
-		// 상태 갱신
-		if (dist >= now_dist) continue;
-		int nx = x + dx[after_dir];
-		int ny = y + dy[after_dir];
+		if (min_dir != 100 && min_dist < cur_dist) {
+			dist_sum += 1;
 
-		bool isdup = false;
-		for (int j = i+1; j <= m; j++) {// 한 칸에 사람 여러명 존재시
-			if (man[j].x == man[i].x && man[j].y == man[i].y) {
-				isdup = true;
-				break;
+			int nx = x + dx[min_dir];
+			int ny = y + dy[min_dir];
+			if (nx == exitxy.first && ny == exitxy.second) {
+				man[i].dead = true;
+			}
+			man[i].x = nx;
+			man[i].y = ny;
+		}
+
+	}
+}
+
+tuple<int, int, int> find_square() {
+	for (int size = 2; size <= n; size++) {
+		// 범위 시작 왼쪽 위 점
+		for (int r = 1; r <= n - size + 1; r++) {
+			for (int c = 1; c <= n - size + 1; c++) {
+				bool mfound = false;
+				bool efound = false;
+				// 인간 체크
+				for (int i = 1; i <= m; i++) {
+					if (man[i].dead) continue;
+
+					if ((r <= man[i].x && man[i].x <= r + size - 1) &&
+						(c <= man[i].y && man[i].y <= c + size - 1)) {
+						mfound = true;
+						break;
+					}
+				}
+
+				// exit 체크
+				if ((r <= exitxy.first && exitxy.first <= r + size - 1) &&
+					(c <= exitxy.second && exitxy.second <= c + size - 1)) {
+					efound = true;
+				}
+
+				if (mfound && efound) {
+					return make_tuple(r, c, size);
+				}
 			}
 		}
-		if (!isdup) {
-			ptable[x][y] = 0;
-		}
+	}
 
-		dist_sum += 1;
+	// 실패 처리
+	return make_tuple(-1, -1, -1);
+}
 
-		if (ptable[nx][ny] == -1) {//출구 좌표라면
-			man[i].done = true;
-		}
-		else {
-			ptable[nx][ny] = i;
+void rotate_man(int x, int y, int size) {
+
+	for (int i = 1; i <= m; i++) {
+		if (man[i].dead) continue;
+		if ((x <= man[i].x && man[i].x <= x + size - 1) &&
+			(y <= man[i].y && man[i].y <= y + size - 1)) {
+
+			int r = man[i].x - x;
+			int c = man[i].y - y;
+
+			int nx = c + x;
+			int ny = -r + size - 1 + y;
+
 			man[i].x = nx;
 			man[i].y = ny;
 		}
 	}
+
+	if ((x <= exitxy.first && exitxy.first <= x + size - 1) &&
+		(y <= exitxy.second && exitxy.second <= y + size - 1)) {
+
+		int r = exitxy.first - x;
+		int c = exitxy.second - y;
+
+		int nx = c + x;
+		int ny = -r + size - 1 + y;
+
+		exitxy.first = nx;
+		exitxy.second = ny;
+	}
 }
 
-tuple<int, int, int> find_square() {// {-1,-1} 리턴할 수도 있음
-	int min_side = 20;
-	int tx = -1;
-	int ty = -1;
-	for (int side = 0; side <= n; side++) {
-		for (int r = 1; r <= n; r++) {
-			for (int c = 1; c <= n; c++) {
-				// 정사각형 안에 man과 exit가 있으면
-				bool man_found = false;
-				bool exit_found = false;
-				int maxr = r + side;
-				int maxc = c + side;
-				if (maxr > n || maxc > n) continue;
+void rotate_wall(int x, int y, int size) {
+	vector<vector<int>> tmp = wtable;
 
-				for (int i = r; i <= r + side; i++) {
-					for (int j = c; j <= c + side; j++) {
-						if (ptable[i][j] > 0) {
-							man_found = true;
-						}
-						else if (ptable[i][j] == -1) {
-							exit_found = true;
-						}
-					}
-				}
-				if (man_found && exit_found && min_side > side) {
-					min_side = side;
-					tx = r;
-					ty = c;
-				}
+	for (int i = x; i <= x + size - 1; i++) {
+		for (int j = y; j <= y + size - 1; j++) {
+			if (wtable[i][j] > 0) {
+				wtable[i][j] -= 1;
 			}
 		}
 	}
 
-	return make_tuple(tx, ty, min_side);
+	for (int i = x; i <= x + size - 1; i++) {
+		for (int j = y; j <= y + size - 1; j++) {
+
+			// (i, j)를 중심 (x, y)에서의 상대 좌표로 변환
+			int r = i - x;
+			int c = j - y;
+			// 시계방향 90도 회전 후 새로운 좌표
+			int nx = c + x;
+			int ny = -r + size - 1 + y;
+
+			tmp[nx][ny] = wtable[i][j];
+		}
+	}
+
+	wtable = tmp;
 }
 
-void rotation(int x, int y, int side) {
-	vector<vector<int>> tmp_table = ptable;
-	vector<vector<int>> tmp_wtable = wtable;
-
-	for (int r = x; r <= x + side; r++) {
-		for (int c = y; c <= y + side; c++) {
-			if (wtable[r][c] > 0) {
-				wtable[r][c] -= 1;
-			}
+void print_wtable() {
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++) {
+			cout << wtable[i][j] << ' ';
 		}
+		cout << '\n';
 	}
+	cout << '\n';
+}
 
-	for (int r = x; r <= x + side; r++) {
-		for (int c = y; c <= y + side; c++) {
-			int ox = r - x;
-			int oy = c - y;
-			int prevx = -(oy - side) + x;
-			int prevy = ox + y;
-
-			int newx = ox + x;
-			int newy = oy + y;
-			tmp_table[newx][newy] = ptable[prevx][prevy];
-			tmp_wtable[newx][newy] = wtable[prevx][prevy];
-
-			int id = tmp_table[newx][newy];
-			if (id > 0) {// 사람 좌표 갱신
-				man[id].x = newx;
-				man[id].y = newy;
-			}
-			else if (id == -1) {
-				exitx = newx;
-				exity = newy;
-			}
-		}
+void print_man() {
+	for (int i = 1; i <= m; i++) {
+		cout << i << " man: " << man[i].x << ' ' << man[i].y << '\n';
 	}
+	cout << '\n';
 
-	ptable = tmp_table;
-	wtable = tmp_wtable;
 }
 
 int main() {
 	cin >> n >> m >> k;
-
 	for (int i = 1; i <= n; i++) {
 		for (int j = 1; j <= n; j++) {
-			int tmp;
-			cin >> tmp;
-			if (tmp > 0) {
-				wtable[i][j] = tmp;
-			}
+			cin >> wtable[i][j];
 		}
 	}
-
 	for (int i = 1; i <= m; i++) {
 		int x, y;
 		cin >> x >> y;
-
 		man[i].x = x;
 		man[i].y = y;
-		ptable[x][y] = i;
 	}
-
-	cin >> exitx >> exity;
-	ptable[exitx][exity] = -1;
+	cin >> exitxy.first >> exitxy.second;
 
 	while (k--) {
-		//cout << "mvoe 전" << '\n';
-		//for (int i = 1; i <= n; i++) {
-		//	for (int j = 1; j <= n; j++) {
-		//		cout << ptable[i][j] << ' ';
-		//	}
-		//	cout << '\n';
-		//}
-		//cout << '\n';
-		
 		//move
-		move();
-		//cout << "move 이후" << '\n';
-		//for (int i = 1; i <= n; i++) {
-		//	for (int j = 1; j <= n; j++) {
-		//		cout << ptable[i][j] << ' ';
-		//	}
-		//	cout << '\n';
-		//}
-		//cout << '\n';
 		
+		//print_man();
+		move();
+		//print_man();
+
 		//find_square
 		tuple<int, int, int> t = find_square();
 		int x = get<0>(t);
 		int y = get<1>(t);
-		int s = get<2>(t);
-		if (x == -1 && y == -1) {
-			break;
-		}
+		int size = get<2>(t);
+		//rotate_man
+		rotate_man(x, y, size);
 
-		//cout << "벽 회전 전:" << '\n';
-		//for (int i = 1; i <= n; i++) {
-		//	for (int j = 1; j <= n; j++) {
-		//		cout << wtable[i][j] << ' ';
-		//	}
-		//	cout << '\n';
-		//}
-		// 
+		//print_wtable();
 		
-		//rotation
-		rotation(x, y, s);
+		//rotate_wall
+		rotate_wall(x, y, size);
 		
-		//cout << "rotation 이후" << '\n';
-		//cout << k << " 턴 종료: " << '\n';
-		//for (int i = 1; i <= n; i++) {
-		//	for (int j = 1; j <= n; j++) {
-		//		cout << ptable[i][j] << ' ';
-		//	}
-		//	cout << '\n';
-		//}
-		//cout << '\n';
-		//for (int i = 1; i <= n; i++) {
-		//	for (int j = 1; j <= n; j++) {
-		//		cout << wtable[i][j] << ' ';
-		//	}
-		//	cout << '\n';
-		//}
+		//print_wtable();
+
 	}
 
 	cout << dist_sum << '\n';
-	cout << exitx << ' ' << exity;
+	cout << exitxy.first << ' ' << exitxy.second;
 }
